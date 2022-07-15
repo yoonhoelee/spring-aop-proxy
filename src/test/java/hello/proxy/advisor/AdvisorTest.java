9a5ed33758e9dead739170444b0001dced3bb80a
yoonhoelee <ubrain0624@gmail.com>
2022-07-15 18:08:23 +0900

    point cut added

---
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,70 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.service.ServiceInterface;
+import hello.proxy.common.service.ServiceInterfaceImpl;
+import hello.proxy.proxy.common.advice.TimeAdvice;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
+@Slf4j
+public class AdvisorTest {
+    @Test
+    void advisorTest1(){
+        ServiceInterface target = new ServiceInterfaceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+        proxy.save();
+        proxy.find();
+    }
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2(){
+        ServiceInterface target = new ServiceInterfaceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+        proxy.save();
+        proxy.find();
+    }
+    static class MyPointCut implements Pointcut{
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MymethodMatcher();
+        }
+    }
+    static class MymethodMatcher implements MethodMatcher{
+        private String matchName = "save";
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트컷 호출 method={} targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과 result={}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
+    }
+}
