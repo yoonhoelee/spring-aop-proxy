c6e242828e4f60c3a4205504fe77bf8f8f84a96f
yoonhoelee <ubrain0624@gmail.com>
2022-07-15 14:25:08 +0900

    jdk 동적 proxy

---
+++ src/test/java/hello/proxy/proxy/cglib/CglibTest.java
@@ -0,0 +1,21 @@
+package hello.proxy.proxy.cglib;
+
+import hello.proxy.common.service.ConcreteService;
+import hello.proxy.proxy.cglib.code.TimeMethodInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+@Slf4j
+public class CglibTest {
+    @Test
+    void cglib(){
+        ConcreteService target = new ConcreteService();
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService)enhancer.create();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+        proxy.call();
+    }
+}
